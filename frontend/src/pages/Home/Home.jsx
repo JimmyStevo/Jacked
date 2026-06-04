4cd4100303835bed5c7cb60f397ee90056ed35bc
James Stevens <jamesrandolphstevens@gmail.com>
2026-06-04 18:57:33 +1200

    GA202-23 adding route pages to app.js and starting work on home page

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import './Home.css'
+
+const Home = () => {
+    return (
+        <>
+        <h1>Hello Style!</h1>
+        <p>Add a little Style!</p>
+        </>
+    );
+}
+
+export default Home;
