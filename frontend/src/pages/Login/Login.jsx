257fab3805e678e1a461ed4d051ff12f41f99a37
Abhey07m <amaisuria45@gmail.com>
2026-06-10 22:27:20 +1200

    Getting node set up

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,13 +1,11 @@
 import './Login.css'
-import MainNavigationBar from '../../components/NavBar/MainNavigationBar';
 import MainButton from '../../components/button/MainButton';
-import SecondNaviationBar from '../../components/NavBar/SecondNavigationBar';
+
 
 const Login = () => {
     return (
         <>
-        <MainNavigationBar/>
-        <SecondNaviationBar/>
+        
         </>
     );
 }
