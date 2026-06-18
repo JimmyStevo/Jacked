46d82a934860602ebf6ee1a3e18016c2d0980629
James Stevens <jamesrandolphstevens@gmail.com>
2026-06-19 09:31:19 +1200

    updating database logic

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
 
 
 client = MongoClient("mongodb+srv://jimmystevo_db_user:TestingDatabase@cluster0.gy3uo1w.mongodb.net/")
-db = client["user_DB"]
+db = client["Jacked_DB"]
 
 print("Connected bitches", db.list_collection_names())
 
-collection = db["INSERT_COLLECTION"]
+user_collection = db["User_Info"]
+preference_collection = db["Preferences"]
 
 
 app = Flask(__name__)
@@ -18,4 +19,7 @@ CORS(app)
 if __name__ == "__main__":
     app.run(debug=True)
     
-    # mongodb+srv://jimmystevo_db_user:IOMbVnxh9nhTspJv@cluster0.gy3uo1w.mongodb.net/?appName=Cluster0
+
+# @app.route('/api/settings', methods=['GET'])
+# def getSettings():
+#     data = list(preference_collection.find{}, )
