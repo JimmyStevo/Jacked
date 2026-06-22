4254138f23ef0c042462807038c54688c8e4e6fc
James Stevens <jamesrandolphstevens@gmail.com>
2026-06-22 19:37:17 +1200

    updating app.js and app.py to handle frontend to backend interactions

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from pymongo import MongoClient
-from auth import auth_bp
+from auth import auth_bp, JWT_SECRET, JWT_ALGORITHM
+import jwt
 
 
 client = MongoClient("mongodb+srv://jimmystevo_db_user:TestingDatabase@cluster0.gy3uo1w.mongodb.net/")
@@ -15,19 +16,24 @@ preference_collection = db["Preferences"]
 
 app = Flask(__name__)
 CORS(app)
-
+def get_current_user():
+    token = request.headers.get('Authorization').split(' ')[1]
+    payload = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+    return payload['sub']
+    
 # ============================================
 # Settings Backend logic
 # ============================================
 
 @app.route('/api/settings', methods=['GET', 'POST'])
 def updateSettings():
+    user_id = get_current_user()
     if request.method == 'POST':
-        data = request.get_json()
-        preference_collection.insert_one(data)
+        data["user_id"] = user_id 
+        preference_collection.update_one(data)
         return jsonify(data)
     else:
-        data = list(preference_collection.find({}, {"_id":0}))
+        data = list(preference_collection.find({"user_id": user_id}, {"_id":0}))
         return jsonify(data)
      
 # ============================================
