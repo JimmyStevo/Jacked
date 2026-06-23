5160658ac5af95e7c5bf3319d192634c2a597858
Abhey07m <amaisuria45@gmail.com>
2026-06-23 18:10:24 +1200

    changed weight logging to foodlogging

--- frontend/src/components/NavBar/SecondNavigationBar.jsx
+++ frontend/src/components/NavBar/SecondNavigationBar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import Cards from '../Cards/Cards';
 import TitleCards from '../Cards/TitleCards';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faGear, faRightFromBracked, faChartLine, faUtensils, faWeightScale, faDumbbell } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faGear, faRightFromBracked, faChartLine, faUtensils, faWeightScale, faDumbbell, faScaleBalanced } from '@fortawesome/free-solid-svg-icons';
 
 const SecondNavigationBar = () => {
     return(
@@ -20,7 +20,7 @@ const SecondNavigationBar = () => {
                     </Link>
                     </li>
                     <li>
-                    <Link to='/weightlogging'> <TitleCards Title={"Weight Logging"} icon={faWeightScale}/>
+                    <Link to='/foodLogging'> <TitleCards Title={"FoodLogging"} icon={faScaleBalanced}/>
                     </Link>
                     </li>
                     <li>
